39f9ab7110826e436609b33d5eb3eb64beb2c3d5
Bakhtiyor <baxtiyorsohibnazarov@gmail.com>
2026-09-20 00:24:49 +0100

    Added custom script and workflow file to test

---
+++ server_scripts/custom.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event =>{
+    event.shaped('minecraft:iron_pickaxe', [
+        'DDD',
+        ' S ',
+        ' S '
+    ], {
+        D: 'minecraft:dirt',
+        S: 'minecraft:stick'
+    })
+})
